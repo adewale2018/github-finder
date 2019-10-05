a3a70edb663cb93eb59781cf2fb31014401b2042
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-10-05 12:16:56 +0100

    Add spinner and refactor the code

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from './../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  render() {
-    const { users } = this.props;
-    const userStyle = {
-      display: "grid",
-      gridTemplateColumns: "repeat(3, 1fr)",
-      gridGap: "1rem"
-    }
+
+const Users = ({ users, loading }) =>  {
+  const userStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    gridGap: "1rem"
+  }
+  if(loading) {
+    return <Spinner />
+  } else {
     return (
       <div style={userStyle}>
         {users.map( user => (
@@ -17,6 +21,10 @@ class Users extends Component {
       </div>
     );
   }
+}
+Users.propType = {
+  users: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 export default Users;
