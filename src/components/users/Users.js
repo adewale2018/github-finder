a1655bfd24f1de10d5a04dd594bfca93f23f9d5e
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-10-05 10:06:13 +0100

    Add the basic components

---
+++ src/components/users/Users.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+
+class Users extends Component {
+  render() {
+    const { users } = this.props;
+    const userStyle = {
+      display: "grid",
+      gridTemplateColumns: "repeat(3, 1fr)",
+      gridGap: "1rem"
+    }
+    return (
+      <div style={userStyle}>
+        {users.map( user => (
+          <UserItem  key={user.id} user={user}/>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Users;
