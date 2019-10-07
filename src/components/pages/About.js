40e8a9041789cfcc553b161f961a1218f3f30133
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-10-07 08:16:42 +0100

    Add router to the app

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About GitHub Users' Finder</h1>
+      <p>This is an app that enables you to search for a GitHub user</p>
+      <p>It is a very nice app built with React. Check it out and have fun with it</p>
+      <p>Version: 1.0.0</p>
+    </Fragment>
+  );
+}
+
+export default About;
